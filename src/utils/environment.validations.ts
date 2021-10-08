a19cdf8240f970070b1337df931a28b1d788dff0
ChristianABINBEV <christian.martinezbarrera-ext@ab-inbev.com>
2021-10-08 09:25:17 -0500

    Update env varibales

--- src/utils/environment.validations.ts
+++ src/utils/environment.validations.ts
@@ -3,10 +3,9 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   APP_NAME: Joi.string().required(),
   APP_AUTHOR: Joi.string().required(),
-  APP_VERSION: Joi.string().required(),
   APP_PORT: Joi.number().required(),
-  APP_ROOT: Joi.string().required(),
   APP_PROD: Joi.boolean().required(),
+  // APP_SECRET: Joi.string().required(), // if use encryption library
   DB_USER: Joi.string().required(),
   DB_PASS: Joi.string().required(),
   DB_NAME: Joi.string().required(),
@@ -24,4 +23,5 @@ export const validationSchema = Joi.object({
   MESSAGE_BROKER_USER: Joi.string().required(),
   MESSAGE_BROKER_PASS: Joi.string().required(),
   LOGS_QUEUE: Joi.string().required(),
+  LOG_EVENT_PATTERN: Joi.string().required(),
 });
